8877d6454239177742e5171b7e29fcac27f15ddc
Meredith Reece <mardebugger@gmail.com>
2018-01-24 23:23:50 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -2,8 +2,8 @@ class Pirate
   attr_reader :name, :weight, :height
 
   def initialize(details)
-    @name = details[:pirate[name]]
-    @weight = details[:pirate[weight]]
-    @height = details[:pirate[height]]
+    @name = details["pirate[name]"]
+    @weight = details["pirate[weight]"]
+    @height = details["pirate[height]"]
   end
 end
