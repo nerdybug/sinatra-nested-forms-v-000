67069fc53872098fc36e85cfc043f6192884df77
Meredith Reece <mardebugger@gmail.com>
2018-01-24 23:09:23 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,5 +1,15 @@
 class Ship
-
+  attr_reader :name, :type, :booty
+  @@all_ships  = []
+  
+  def initialize(details)
+    @name = params[:pirate[ships][][name]]
+    @type = params[:pirate[ships][][type]]
+    @booty = params[:pirate[ships][][booty]]
+    
+    @@all_ships << self
+  end
+  
   def self.clear
 
   end
