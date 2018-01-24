5078a8ac7039e21d20f844f26bedf89a6dcaf2e0
Meredith Reece <mardebugger@gmail.com>
2018-01-24 23:34:47 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,9 @@
 class Pirate
+  attr_reader :name, :weight, :height
+
+  def initialize(details)
+    @name = details[:name]
+    @weight = details[:weight]
+    @height = details[:height]
+  end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+  attr_reader :name, :type, :booty
+  @@all_ships  = []
+
+  def initialize(details)
+    @name = details[:name]
+    @type = details[:type]
+    @booty = details[:booty]
+
+    @@all_ships << self
+  end
+
+  def self.all
+    @@all_ships
+  end
+
+  def self.clear
+    @@all_ships.clear
+  end
 end
